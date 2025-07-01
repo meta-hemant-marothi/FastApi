5f38b736fe139455e58af93b2814a71322a2b165
hemant marothi <hemant.marothi@metacube.com>
2025-07-01 14:31:55 +0530

    updates

--- blog/schemas.py
+++ blog/schemas.py
@@ -29,3 +29,14 @@ class ShowBlog(BaseModel):
 
     class Config():
         orm_mode = True
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
