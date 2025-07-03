b6e814641e177e3fed74bd8b33479f52675f7312
meta-hemant-marothi <hemant.marothi@metacube.com>
2025-07-03 12:11:40 +0530

    updated

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from .. import database, schemas, models
+from blog import database, schemas, models
 from sqlalchemy.orm import Session
-from fastapi import APIRouter,Depends,status
-from ..repository import user
+from fastapi import APIRouter, Depends, status
+from blog.repository import user
 
 router = APIRouter(
     prefix="/user",
@@ -13,9 +13,10 @@ get_db = database.get_db
 
 
 @router.post('/', response_model=schemas.ShowUser)
-def create_user(request: schemas.User,db: Session = Depends(get_db)):
-    return user.create(request,db)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    return user.create(request, db)
 
-@router.get('/{id}',response_model=schemas.ShowUser)
-def get_user(id:int,db: Session = Depends(get_db)):
-    return user.show(id,db)
+
+@router.get('/{id}', response_model=schemas.ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return user.show(id, db)
--- blog/schemas.py
+++ blog/schemas.py
@@ -30,13 +30,16 @@ class ShowBlog(BaseModel):
     class Config():
         orm_mode = True
 
+
 class Login(BaseModel):
     username: str
-    password: str
+    password:str
+
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class TokenData(BaseModel):
-    username: Optional[str] = None
+    email: Optional[str] = None
